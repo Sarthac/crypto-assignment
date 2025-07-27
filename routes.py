45e7e5311ffe6fe05f5952116abc0d4fb1d42614
sarthac <hi99@tutanota.com>
2025-07-28 00:00:17 +0530

    rm method ceasar_cipher that doesn't need

--- routes.py
+++ routes.py
@@ -1,19 +1,5 @@
 import string, random
 
-def caesar_cipher(text: str, shift: int = 3) ->str :
-    result = ''
-    for char in text:
-        if char.isupper():
-            # capital letter in ASCII start from 97, in ASCII A = 65
-            # I would like to set letter A to 1 so -65 and to calculate modular arthmatic using moduls
-            result += chr((ord(char) - 65 + shift) % 26 + 65)
-        elif char.islower():
-            # small letter in ASCII start from 97, in ASCII a = 97
-            result += chr((ord(char) - 97 + shift) % 26 + 97)
-        else:
-            result += char
-    return result
-
 class SubstitutionCipher:
 
     def __init__(self, cipher_alphabets: list) -> None:
